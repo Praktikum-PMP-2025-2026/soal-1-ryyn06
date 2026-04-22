f76adfbd05fcfb131ec46a5c08eae2f404456c37
ryyn06 <rayyanhwr@gmail.com>
2026-04-22 09:17:17 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,60 @@
 #include <stdio.h>
+#define max_len 99
 
-int main(void) {
-    return 0;
+void sorting(float *arr[max_len], int len, float *SUM, float *AVG, float *Max, float *IDX){
+    //menggunakan bubble sort
+
+    float temp;
+    for (size_t i = 0; i < len; i++)
+    {
+        for (size_t j = 0; j < len-i-1 ; j++)
+        {
+            if ((*arr[j] > (*arr[j+1])))
+            {
+                temp = (*arr[j]);
+                *arr[j] = *arr[j+1];
+                *arr[j+1] = temp;
+            }
+            
+        }
+        
+    }
+
+    *SUM = 0;
+
+    for (size_t i = 0; i < len; i++)
+    {
+        *SUM += *arr[i];
+    }
+
+  
+
+    *Max =  *arr[len-1];
+    *IDX = *arr[0];
+    *AVG = *SUM / len;
+
+
+    printf("SUM %.0f\n", *SUM);
+    printf("AVG %.2f\n", *AVG);
+    printf("MAX %.0f\n", *Max);
+    printf("IDX %.0f\n", *IDX );
+    
+}
+
+int main(){
+    float SUM, AVG, MAX,IDX;
+    float arr[max_len];
+    float *ptr[max_len];
+    int len;
+
+    scanf("%d", &len);
+
+    for (int i = 0; i < len; i++) {
+        scanf("%f", &arr[i]);
+        
+        ptr[i] = &arr[i];
+    }
+
+    sorting(ptr, len, &SUM, &AVG, &MAX, &IDX);
+
 }
